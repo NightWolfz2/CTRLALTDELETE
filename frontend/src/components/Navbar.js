3694f199d4cf2f91ae5f81602663dcb7cd2682e1
Arjun B <abhargava@csus.edu>
2023-10-26 16:06:46 -0700

    JIRA/NM-117
    Updated Navbar Design

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,23 +1,24 @@
-import { Link } from 'react-router-dom'
-const Navbar = () => {
-    
-    return(
-        <header>
-            <div className="container">
-                <h1>Code Ninjas</h1>
-                <nav className="navList">
-                    <ul className="ulList">
-                        <li className="navItem"><Link to="/">Home</Link></li>
-                        <li className="navItem"><Link to="/createTask">Create Task</Link></li>
-                        <li className="navItem"><Link to="/calendar">Calendar</Link></li>
-                        <li className="navItem"><Link to="/calendar">Calendar</Link></li>
-                        <li className="navItem"><Link to="/overview">Overview</Link></li>
-                        <li className="navItem"><Link to="/history">History</Link></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    )
-}
+import { Link } from 'react-router-dom';
 
-export default Navbar
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+             <div className="logo-title-container">
+            <img src="https://res.cloudinary.com/micronetonline/image/upload/c_crop,h_362,w_780,x_0,y_0/v1580249584/tenants/f7ad7557-d52d-4af2-bcb5-ec334f8fc6b6/59a7c5ec6587427db30d47fa237c7a85/CN-logo-blk-backgrnd.png" alt="Logo" className="logo" />
+            <h1 style={{ marginLeft: '50px' }}>Ninja Manager</h1>
+
+            </div>
+            <div className="links">
+                <Link to="/">Home</Link>
+                <Link to="/create-task">Create Task</Link>
+                <Link to="/calendar">Calendar</Link>
+                <Link to="/overview">Overview</Link>
+                <Link to="/history">History</Link>
+                <Link to="/profile">Profile</Link>
+                <Link to="/menu">Menu</Link>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
