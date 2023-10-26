ca6ddf8dca47da22d90ae6479b0ed534d68af3dc
johnvardanyan <johnvardanyan@csus.edu>
2023-10-26 12:38:39 -0700

    Backend Skeleton
    Back end skeleton of the website with a link to MongoDB

---
+++ backend/models/taskModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const taskSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  date: { 
+    type: String, //change this later to Date format
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  priority: {
+    type: Number, // change this to High,Low, etc.
+    required: false
+  }
+}, { timestamps: true })
+
+module.exports = mongoose.model('Task', taskSchema)
