ebdc62d4a0886835e7dca7122af25413da505e31
egrigoryan12 <elina.grigoryan8@gmail.com>
2023-10-31 17:41:22 -0700

    JIRA NM-95, NM-96, NM-97 - Filters
    Added Filter by Priority (Low, Medium, High), Filter by Status (In Progress, Past Due), and Filter by Due Date w/ calendar.

--- frontend/src/pages/Overview.js
+++ frontend/src/pages/Overview.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+/*import { useEffect } from "react"
 import { useTasksContext } from "../hooks/useTasksContext"
 
 const Overview = () => { 
@@ -8,3 +8,67 @@ const Overview = () => {
 }
 
 export default Overview
+
+*/
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "../../src/index.css";
+const Overview = () => {
+  const [priorityLevel, setPriorityLevel] = useState("All");
+  const [status, setStatus] = useState("All");
+  const [dueDate, setDueDate] = useState(null);
+
+  const priorityChange = (e) => {
+    setPriorityLevel(e.target.value);
+  };
+
+  const statusChange = (e) => {
+    setStatus(e.target.value);
+  };
+
+  const duedateChange = (date) => {
+    setDueDate(date);
+  };
+
+  const clearFilters = () => {
+    setPriorityLevel("All");
+    setStatus("All");
+    setDueDate(null);
+  };
+
+  return (
+    <div>
+    <div className="priority-label">
+      <label>Filter by Priority:</label>
+      <select className="priority-select" value={priorityLevel} onChange={priorityChange}>
+        <option value="All">All</option>
+        <option value="Low">Low</option>
+        <option value="Medium">Medium</option>
+        <option value="High">High</option>
+      </select>
+    </div>
+  
+    <div className="status-label">
+      <label>Filter by Status:</label>
+      <select className="status-select" value={status} onChange={statusChange}>
+        <option value="All">All</option>
+        <option value="In Progress">In Progress</option>
+        <option value="Past Due">Past Due</option>
+      </select>
+    </div>
+    <div className="due-date-label">
+      <label>Filter by Due Date:</label>
+      <DatePicker className="due-date-select" selected={dueDate} onChange={duedateChange} />
+    </div>
+  
+    <div className="clear-button">
+      <button onClick={clearFilters}>Clear Filters</button>
+    </div>
+  </div>
+  
+
+  );
+};
+
+export default Overview;
