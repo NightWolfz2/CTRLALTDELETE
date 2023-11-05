58268b9526202293b906fcc9c4a8b6ad9b75676d
NikolayChkhaylo <112992720+NikolayChkhaylo@users.noreply.github.com>
2023-11-04 19:11:30 -0700

    JIRA/NM-102, 104
    Implemented the drop down for filter by priority and search by text textbox.

--- frontend/src/pages/History.js
+++ frontend/src/pages/History.js
@@ -1,10 +1,53 @@
+import React, { useState } from "react";
 import { useEffect } from "react"
 import { useTasksContext } from "../hooks/useTasksContext"
 
-const History = () => { 
-    return (
-        <h2>History</h2>
-      )
-}
+const History = () => {
+  const [selectedPriority, setSelectedPriority] = useState("");
+  const [textFilter, setTextFilter] = useState("");
+  const [dueDate, setDueDate] = useState("");
+
+  const handlePriorityChange = (event) => {
+    setSelectedPriority(event.target.value);
+  };
+
+  const handleTextFilterChange = (event) => {
+    setTextFilter(event.target.value);
+  };
+
+  const duedateChange = (date) => {
+    setDueDate(date);
+  };
+
+  return (
+    <div>
+      <div className="hpriority-label">
+        <label >Filter by Priority:</label>
+        <select className="text-filter-input"
+          value={selectedPriority}
+          onChange={handlePriorityChange}
+        >
+          <option value="">All</option>
+          <option value="Low">Low</option>
+          <option value="Medium">Medium</option>
+          <option value="High">High</option>
+        </select>
+      </div>
+      
+      <div className="hpriority-textlabel">
+        <label> Filter by Text:</label>
+        <input className="hpriority-textinput"
+          type="text"
+          value={textFilter}
+          onChange={handleTextFilterChange}
+          style={{ width: "96px" }}
+        />
+      </div>
+
+     
+
+    </div>
+  );
+};
 
 export default History
