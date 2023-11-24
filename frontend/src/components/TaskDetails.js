1cfcd50e951064c4e10a14ad50195956cbb47891
Navid Baghaei <navidbaghaei@csus.edu>
2023-11-23 21:48:31 -0800

    JIRA/NM-153, NM- 162, NM- 163
    #153 Changed the create task page to allow the user to select a time under the due date field.
    
    #162 Fixed issue where task description would extend infinitely to the right on the home page
    
    #163 Changed the TaskDetails.js file to display the due time in addition to the due date on the Home page.

--- frontend/src/components/TaskDetails.js
+++ frontend/src/components/TaskDetails.js
@@ -12,16 +12,27 @@ const TaskDetails = ({ task }) => {
       dispatch({type: 'DELETE_TASK', payload: json})
     }
   };
+
   var taskDate = new Date(task.date);
+
+  // Extract and format the time components in 12-hour format with AM/PM
+  var hours = taskDate.getHours();
+  var minutes = taskDate.getMinutes().toString().padStart(2, '0');
+  var ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  hours = hours.toString().padStart(2, '0');
+
   var day = String(taskDate.getDate()).padStart(2, '0');
-  var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 
+                    'July', 'August', 'September', 'October', 'November', 'December'];
   var month = monthNames[taskDate.getMonth()];
   var year = taskDate.getFullYear();
-  var formattedDate = `${day} ${month} ${year}`;
-  
+  var formattedDate = `${day} ${month} ${year} ${hours}:${minutes} ${ampm}`;
 
   const statusClassName = `status-${task.status.replace(/\s+/g, '-').toLowerCase()}`;
   
+
   return (
     <div className="task-details">
       <div className="task-header">
@@ -30,16 +41,16 @@ const TaskDetails = ({ task }) => {
       </div>
       <div className="task-info">
         <p><strong>Due Date: </strong>{formattedDate}</p>
-
-        {/* Separate the label and content for description */}
-        <p className="description"><strong>Description:</strong></p>
-        <p>{task.description}</p>
+        <div className="description">
+          <strong>Description:</strong>
+          <p className="task-desc-text">{task.description}</p> {/* This is the updated part */}
+        </div>
         <p className="priority"><strong>Priority: </strong>{task.priority}</p>
         <p><strong>Assigned: </strong></p>
         <button type="button" className="delete-button" onClick={deleteClick}><span>delete</span></button>
       </div>
-      
     </div>
   );
 }
+
 export default TaskDetails;
