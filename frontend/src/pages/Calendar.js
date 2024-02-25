26fd0f35c90de5f6c589de645e1a8a8b4505c979
Jeremy Vuong <jeremyvuong1995@gmail.com>
2024-02-24 21:00:33 -0800

    NM-233 quick fix

--- frontend/src/pages/Calendar.js
+++ frontend/src/pages/Calendar.js
@@ -21,16 +21,6 @@ const CalendarPage = () => {
   const navigate = useNavigate(); 
   const { logout } = useLogout(); 
 
-  function formatDateTime(date) {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-  
-    return `${year}-${month}-${day}T${hours}:${minutes}`;
-  }
-
   useEffect(() => {
     const fetchTasks = async () => {
       try {
