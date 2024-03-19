855269cb1b8a0090156f3b3c66f6525a73d7bfd2
Jeremy Vuong <jeremyvuong1995@gmail.com>
2024-03-18 23:00:57 -0700

    Ignore
    Code transfer

--- frontend/src/pages/Calendar.js
+++ frontend/src/pages/Calendar.js
@@ -8,17 +8,13 @@ import { useLogout } from '../hooks/useLogout';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import './../css/Calendar.css';
-import TaskDetails2 from '../components/TaskDetails';// Confirm the path is correct
 import { useAuthContext } from '../hooks/useAuthContext';
-import { useTasksContext } from '../hooks/useTasksContext';
+
 
 const localizer = momentLocalizer(moment);
 
 const CalendarPage = () => {
-  const { dispatch } = useTasksContext();
-
   const [tasks, setTasks] = useState([]);
-  const [showPopup, setShowPopup] = useState(false);
   const [selectedTask, setSelectedTask] = useState(null);
   const [tasksDue, setTasksDue] = useState([]);
   //Dates
@@ -27,7 +23,10 @@ const CalendarPage = () => {
   //
   const [showTasks, setShowTasks] = useState(true);
   const [showTaskInfo, setShowTaskInfo] = useState(false);
+  //
+  const [agendaDate, setAgendaDate] = useState('');
   const [isEditing, setIsEditing] = useState(false);
+
   const [employees, setEmployees] = useState([]);
   const customFetch = useCustomFetch();
   const navigate = useNavigate();
@@ -35,6 +34,84 @@ const CalendarPage = () => {
   const { user } = useAuthContext();
   const [currentEmployee, setCurrentEmployee] = useState('');
 
+  const minTime = new Date();
+  minTime.setHours(6, 0, 0); // Set minimum time to 6:00 AM
+
+  const maxTime = new Date();
+  maxTime.setHours(22, 0, 0); // Set maximum time to 10:00 PM
+
+  useEffect(() => {
+    // Update agenda text whenever date changes
+    setAgendaDate(`${date.toLocaleDateString('en-CA', { month: 'long' })} ${date.getDate()}, ${date.getFullYear()}`);
+  }, [date]);
+
+  // Filtering 
+  // Example button click handler
+  const handleFilterButtonClick = () => {
+
+  };
+  // Filter tasks based on the selected date
+  const filterTasksByDate = (tasksArray, selectedDate) => {
+    const filtTasks = tasksArray.filter(task => {
+      const taskDueDate = moment(task.date).startOf('day').toDate(); // Convert task due date to start of day
+      return moment(taskDueDate).isSame(selectedDate, 'day'); // Check if task due date is the same as selected date
+    });
+
+    setTasksDue(filtTasks);
+  };
+
+
+  const handleDateChange = (date) => {
+    //set new date
+    const selected = new Date(moment(date).toDate());
+    setDate(selected);
+    //close task info
+    handleCloseTaskInfo();
+    //filter by date
+    const filtTasks = filterTasksByDate(tasks, date);
+    setShowTasks(true);
+  };
+
+  const handleNavigate = (newDate) => {
+    setDate(newDate);
+    filterTasksByDate(tasks, newDate);
+    setShowTaskInfo(false);
+    setShowTasks(true);
+  };
+
+  const EventComponent = ({ event }) => {
+    const { title, priority, status, end } = event;
+    const eventStyle = {
+      backgroundColor: getTaskBgColor(priority),
+      opacity: status === 'Past Due' ? 0.5 : 1,
+      color: 'black',
+      height: '100%',
+      padding: '0 2px',
+      paddingTop: '3px'
+    };
+
+    return (
+      <div style={eventStyle}>
+        <span>{moment(end).format('LT')}: {title}</span>
+      </div>
+    );
+  };
+
+  // Function to get background color of task based on priority
+  const getTaskBgColor = (priority) => {
+    switch (priority) {
+      case 'High':
+        return '#ffad99';
+      case 'Medium':
+        return '#f3f899';
+      case 'Low':
+        return '#adebad';
+      default:
+        return '#3174ad'; // Default color if priority is not defined
+    }
+  };
+
+  // Fetch employees
   useEffect(() => {
     const fetchEmployees = async () => {
       if (!user) return;
@@ -63,23 +140,25 @@ const CalendarPage = () => {
     fetchEmployees();// Dependency array, if you're using the 'user' state to store user information
   }, [user]);
 
-  const minTime = new Date();
-  minTime.setHours(6, 0, 0); // Set minimum time to 6:00 AM
-
-  const maxTime = new Date();
-  maxTime.setHours(22, 0, 0); // Set maximum time to 10:00 PM
-
-
+  // Fetch Tasks
   useEffect(() => {
     const fetchTasks = async () => {
       try {
         // Fetch tasks from database API
         const data = await customFetch('/api/tasks');
 
+        /* const allTasks = data
+            .filter(task => new Date(task.date) >= today)
+            .map(task => ({
+            ...task,
+            start: moment(task.date).startOf('day').toDate(),
+            end: moment(task.date).toDate(),
+          }));
+*/
         const allTasks = data.map(task => ({
             //.filter(task => new Date(task.date) >= now)
             ...task,
-            start: moment(task.date).startOf('day').toDate(),
+            start: moment(task.date).toDate(),
             end: moment(task.date).toDate(),
           }));
 
@@ -97,31 +176,17 @@ const CalendarPage = () => {
     fetchTasks();
   }, []);
 
-  const filterTasksByDate = (tasksArray, selectedDate) => {
-    // Filter tasks based on the selected date
-    const filteredTasks = tasksArray.filter(task => {
-      const taskDueDate = moment(task.start).startOf('day').toDate(); // Convert task due date to start of day
-      return moment(taskDueDate).isSame(selectedDate, 'day'); // Check if task due date is the same as selected date
-    });
-
-    return filteredTasks;
-  };
-
-  const handleEventClick = task => {
+  const handleTaskClick = task => {
     setSelectedTask(task);
-    setShowPopup(true);
     setShowTasks(false);
     setShowTaskInfo(true);
   };
 
-  const handleClosePopup = () => {
-    setShowPopup(false);
-  };
 
   const handleCloseTaskInfo = () => {
     setShowTaskInfo(false);
     setShowTasks(true);
-    setSelectedTask();
+    setSelectedTask(null);
   };
 
   const handleEditTask = () => {
@@ -135,9 +200,6 @@ const CalendarPage = () => {
     const json = customFetch(`/api/tasks/${selectedTask._id}`, 'PATCH', selectedTask, user.token);
   };
 
-  const handleCloseDetails = () => {
-    setSelectedTask(null); // Set selectedTask to null to close the TaskDetails window
-  };
 
   const handleTitleChange = (e) => {
     setSelectedTask({
@@ -201,77 +263,11 @@ const CalendarPage = () => {
     }
   };
 
-  const handleDateChange = (date) => {
-    //set new date
-    const selected = new Date(moment(date).toDate());
-    setDate(selected);
-    //close task info
-    handleCloseTaskInfo();
-    //filter by date
-    const filteredTasks = filterTasksByDate(tasks, date);
-    setTasksDue(filteredTasks)
-    setShowTasks(true);
-  };
-
-  const components = {
-    event: (props) => {
-      const { title, priority, status } = props?.event;
-      // Combine styles for past due tasks and priority-based styling
-      const combinedStyles = {
-        opacity: status === 'Past Due' ? 0.5 : 1,
-        backgroundColor: getTaskBgColor(priority),
-        color: 'black'
-      };
-
-      return (
-        <div style={combinedStyles}>
-          <div>{title}</div>
-          <div>
-            {employees.map(employee => (
-              <span key={employee._id} value={employee._id}>
-                {employee.fname.charAt(0)}. {employee.lname}
-              </span>
-            ))}
-          </div>
-        </div>
-      );
-    }
-  };
-
-  // Function to get background color based on priority
-  const getTaskBgColor = (priority) => {
-    switch (priority) {
-      case 'High':
-        return '#ffad99';
-      case 'Medium':
-        return '#f3f899';
-      case 'Low':
-        return '#adebad';
-      default:
-        return '#3174ad'; // Default color if priority is not defined
-    }
-  };
-
   /* */
 
 
   return (
     <div className="calendar-page">
-      <Calendar
-      style={{height:"90vh"}}
-        localizer={localizer}
-        views={['month', 'week', 'day', 'agenda']}
-        events={tasks}
-        components={components}
-        startAccessor="start" // Use 'start' as the accessor for the start date
-        endAccessor="end" // Use 'end' as the accessor for the end date
-        defaultDate={new Date()}
-        date={date}
-        min={minTime}
-        max={maxTime}
-        //eventPropGetter={eventStyleGetter} // Apply custom styles to events
-        onSelectEvent={handleEventClick} // Handle event click
-      />
 
       <div className="side-container">
 
@@ -288,15 +284,24 @@ const CalendarPage = () => {
         </div>
         <div className="side-component-2">
         {showTasks && (
-            <div className="task-info">
-              <h3>Tasks</h3>
-              <ul>
-                {tasksDue.map(task => (
-                <li key={task.id}>
-                  {task.title}
-                </li>
-                ))}
-              </ul>
+            <div className="task-info" >
+              <h3 >
+                Agenda: {agendaDate}
+              </h3>
+              <div className="task-list">
+                <ol style={{ listStyleType: 'none', width: '100%', padding: 0, backgroundColor: '#353535'}}>
+                  {tasksDue.slice().sort((a, b) => new Date(a.date) - new Date(b.date)).map(task => (
+                    <li key={task.id} style={{ marginRight: '40px'}}>
+                      <div style={{marginRight: '30px'}}>
+                        {new Date(task.date).toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', hour12: true, minimumIntegerDigits: 2 })}
+                      </div>
+                      <div style={{ width: '70%', color: 'black', backgroundColor: getTaskBgColor(task.priority), paddingRight: ''}}>
+                        {task.title}
+                      </div>
+                    </li>
+                  ))}
+                </ol>
+              </div>
             </div>
           )}
         {showTaskInfo && (
@@ -317,7 +322,7 @@ const CalendarPage = () => {
                   readOnly={!isEditing}
                   className={isEditing ? '' : 'read-only'} // Apply 'read-only' class when not editing
                   type="datetime-local"
-                  value={selectedTask.date}
+                  value={moment(selectedTask.date).format("YYYY-MM-DDTHH:mm")}
                   onChange={handleDateInputChange}
                 >
                 </input>
@@ -330,9 +335,9 @@ const CalendarPage = () => {
                   value={selectedTask.priority}
                 >
                   <option value="">Select Priority</option>
-                  <option value="high">High</option>
-                  <option value="medium">Medium</option>
-                  <option value="low">Low</option>
+                  <option value="High">High</option>
+                  <option value="Medium">Medium</option>
+                  <option value="Low">Low</option>
                 </select>
 
                 <label>Assignee:</label>
@@ -390,6 +395,27 @@ const CalendarPage = () => {
           )}
         </div>
       </div>
+      <Calendar
+        style={{height:"90vh"}}
+        localizer={localizer}
+        views={['month', 'week', 'day']}
+        defaultView="week"
+        events={tasks}
+        components={{
+          event: EventComponent,
+        }}
+        formats={{
+          eventTimeRangeFormat: () => null, // Remove the time label from week and day view
+        }}
+        startAccessor="start" // Use 'start' as the accessor for the start date
+        endAccessor="end" // Use 'end' as the accessor for the end date
+        defaultDate={today}
+        date={date}
+        min={minTime}
+        max={maxTime}
+        onSelectEvent={handleTaskClick} // Handle event click
+        onNavigate={handleNavigate} // Pass handleNavigate to the Calendar component
+      />
     </div>
   )
 }
